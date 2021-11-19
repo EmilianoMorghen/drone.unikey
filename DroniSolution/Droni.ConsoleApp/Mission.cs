c30c91b3e6e3ab3899d70b312afebab132967e9a
LAPTOP-NKAO4QSS\Luca <lucapaniconi.2000@libero.it>
2021-11-19 17:04:41 +0100

    Creazione classe Mission

---
+++ DroniSolution/Droni.ConsoleApp/Mission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Droni.ConsoleApp
+{
+    internal class Mission
+    {
+        public int Id { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public List<Aircraft> Aircrafts { get; set; } = new List<Aircraft>();
+
+        public List<Flight> Flights { get; set; } = new List<Flight>();
+    }
+}
