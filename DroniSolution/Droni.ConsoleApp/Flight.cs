f19834055d6b7c26364550946207597c88449b4d
Emiliano Morghen <emiliano.morghen@gmail.com>
2021-11-19 16:44:00 +0100

    Creata classe Flight e le sue properties

---
+++ DroniSolution/Droni.ConsoleApp/Flight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Droni.ConsoleApp
+{
+    internal class Flight
+    {
+        public string ID { get; }
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; }
+        public Aircraft aircraft { get; }
+    }
+}
