e79f16c1bb66a7b1118f032e9670057e2018d832
LAPTOP-NKAO4QSS\Luca <lucapaniconi.2000@libero.it>
2021-11-23 17:18:43 +0100

    Add Method DetailsOfMission

---
+++ DroniSolution/Droni.ConsoleApp/DroneFleet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Droni.ConsoleApp
+{
+    internal class DroneFleet
+    {
+        private List<Mission> _missions = new List<Mission>();
+
+        public DroneFleet(List<Mission> missions)
+        {
+            _missions = missions;
+        }
+
+        public Mission? DetailsOfMission(int id) => _missions.Find(mission => mission.Id == id);
+
+    }
+}
